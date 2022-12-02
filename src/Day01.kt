59419e30be93bb6abef18997b94af546f33a0423
yashu <yashu@lucky5.dev.com>
2022-12-02 21:29:38 +1100

    Solution to day1 puzzle

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,38 @@
+import java.util.*
+
+
+private fun getSortedEnergyLevels(input: List<String>) : List<Int> {
+
+    val energyLevels = mutableListOf<Int>()
+
+    val result = mutableListOf<Int>()
+
+    input.forEach {
+        if (it.isNotEmpty()) {
+            energyLevels.add(it.toInt())
+        } else {
+            result.add(energyLevels.sum())
+            energyLevels.clear()
+        }
+
+    }
+
+    return result.sortedDescending()
+}
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val energyLevels = getSortedEnergyLevels(input)
+        return energyLevels[0]
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val energyLevels = getSortedEnergyLevels(input)
+        return energyLevels[0] + energyLevels[1] + energyLevels[2]
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
 
     val input = readInput("Day01")
     println(part1(input))
